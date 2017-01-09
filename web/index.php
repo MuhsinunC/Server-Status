056b9277b9202d790878734c18a77cc006532f17
Muhsinun Chowdhury <muhsinun08@gmail.com>
2017-01-08 22:45:27 -0500

    pretty much done for the night

--- web/index.php
+++ web/index.php
@@ -5,23 +5,22 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<!-- Load Font Awesome -->
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-	<!-- Load Yeti Theme DISABLED
-	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css" rel="stylesheet" integrity="sha384-HzUaiJdCTIY/RL2vDPRGdEQHHahjzwoJJzGUkYjHVzTwXFQ2QN/nVgX7tzoMW3Ov" crossorigin="anonymous">
 	<!-- Load Bootstrap 4.0.0 Alpha 6 -->
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<!-- Load Yeti Theme DISABLED
+	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css" rel="stylesheet" integrity="sha384-HzUaiJdCTIY/RL2vDPRGdEQHHahjzwoJJzGUkYjHVzTwXFQ2QN/nVgX7tzoMW3Ov" crossorigin="anonymous">
 	<!-- Load Custom CSS -->
 	<link href="./res/css/custom.css">
 </head>
 <body>
-	<div class="container" style="width: 20rem;">
+	<div class="container">
 		<div class="card">
 			<div class="card-header">
-				<span class="h4 card-title">Card title</span>
+				<span class="h4 card-title">Servers</span>
 			</div>
 			<div class="card-block">
-				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				<a href="#" class="btn btn-primary">Go somewhere</a>
+				<p class="card-text">Your servers will be displayed below</p>
 			</div>
 		</div>
 	</div>
