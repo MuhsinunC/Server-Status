d0abb7c108528189c6b83d3e3028cc0ba83a73dd
Muhsinun Chowdhury <muhsinun08@gmail.com>
2017-01-08 22:39:59 -0500

    setup card

--- web/index.php
+++ web/index.php
@@ -5,17 +5,25 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<!-- Load Font Awesome -->
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-	<!-- Load Yeti Theme
+	<!-- Load Yeti Theme DISABLED
 	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css" rel="stylesheet" integrity="sha384-HzUaiJdCTIY/RL2vDPRGdEQHHahjzwoJJzGUkYjHVzTwXFQ2QN/nVgX7tzoMW3Ov" crossorigin="anonymous">
-	TEMP DISABLED
-	-->
-	<!-- Load Bootstrap 4.0.0 Alpha -->
+	<!-- Load Bootstrap 4.0.0 Alpha 6 -->
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<!-- Load Custom CSS -->
+	<link href="./res/css/custom.css">
 </head>
 <body>
-	<div class="container">
-		<h1>Test Header!</h1>
+	<div class="container" style="width: 20rem;">
+		<div class="card">
+			<div class="card-header">
+				<span class="h4 card-title">Card title</span>
+			</div>
+			<div class="card-block">
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
