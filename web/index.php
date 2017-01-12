676f4195cbd86d7195f4bfe96916cbee98b3aaf7
Muhsinun Chowdhury <muhsinun08@gmail.com>
2017-01-11 23:57:52 -0500

    pretty much done! added all info relaying & nearly done theming! :D

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@
 			
 			<?php
 			
-				foreach ($configs->servernames as $key => $servertitle) {
+				foreach ($configs->servernames as $key => $servername) {
 					
 					$serverip =  $configs->serverips[$key];
 					$serverport =  $configs->serverports[$key];
@@ -18,23 +18,114 @@
 						$json = file_get_contents('https://mcapi.us/server/status?ip=' . $serverip . '&port=' . $serverport);
 						$data = json_decode($json,true);
 					
-						$onlinestatus = $data['online'];
-					
+						$status = $data['status']; //Success or failure getting info (Ex. success)
+						$online = $data['online']; //Server Online or not (Ex. true)
+						$motd = $data['motd']; //Server MOTD (Ex. A Minecraft Server, Some strange formatting may be in
+											   //this, there are various libraries available for formatting this.)
+						$error = $data['error']; //Error message from the request (Error Messages: If you forget the IP
+												 //the error will be missing data. if it is an invalid IP, the error will
+												 //be invalid hostname or port. if an error starts with internal server
+												 //error, it means something is wrong with my server. empty means no error.)
+						$maxplayers = $data['players']['max']; //Maximum player limit (Ex. 20)
+						$currentplayers = $data['players']['now']; //Current number of players (Ex. 2)
+						$serverversion = $data['server']['name']; //Current server version name (Ex. Spigot 1.11.2)
+						$serverprotocol = $data['server']['protocol']; //Server version protocol (Ex. 47)
+						$lastonlinestamp = $data['last_online']; //The date the server was last recorded online. If empty, it has
+															//never been online. it is a unix timestamp in string form. if
+															//this has the same value as last_updated, it means that it is
+															//currently online. (Ex. 1431985691)
+						$lastupdatedstamp = $data['last_updated']; //The date the status of the server was last updated at. It
+															  //updates every five minutes, so you may send requests as
+															  //soon as it has expired. (Ex. 1431985691)
+															  
+					//Convert Unix Timestamps To Readable Datetimes
+						$lastonline = date('d-m-Y H:i:s', intval($lastonlinestamp));
+						$lastupdated = date('d-m-Y H:i:s', intval($lastupdatedstamp));
+						
 					echo '
 					
 					<li>
-						<div class="collapsible-header"><i class="material-icons">'. 
+						<div class="collapsible-header"><i class="material-icons">';
 						
-							//Online or offline response
-								(($onlinestatus == 1)?'check_circle':"cancel")
+							//Online or offline response OLD (Ternary): (($online == 1)?'check_circle':"cancel")
+								
+								if ($online == 1) {
+									
+									echo "check_circle";
+									
+								}
+								else {
+									
+									echo "cancel";
+									
+								}
 							
-							.'</i>'. $serverip .'</div>
-						<div class="collapsible-body"><p>Server Info & Status</p></div>
+							echo '</i>'. $servername .'<span class="secondary-content">';
+							
+								if ($online == 1) {
+									
+									echo $currentplayers . '/' . $maxplayers;
+									
+								}
+								else {
+									
+									echo "Offline";
+									
+								}
+							
+							echo '</span></div>
+						<div class="collapsible-body"><p>';
+						
+							if ($online == 1) {
+								
+								echo '
+								
+									Online
+									<br>
+									Motd: '. $motd .'
+									<br>
+									Players: '. $currentplayers .'/'. $maxplayers .'
+									<br>
+									Server Version: '. $serverversion .'';
+									
+										if (!$lastonline == NULL) {
+											
+											echo '<br>Last Online: '. $lastonline .'';
+											
+										}
+										
+										if (!$lastupdated == NULL) {
+											
+											echo '<br>Last Updated: '. $lastupdated .'';
+											
+										}
+									
+									'
+									
+								
+								';
+								
+							}
+							else {
+								
+								if ($error == 'missing data' | $error == 'invalid hostname or port' | substr($error, 0, strlen($error)) === 'internal server error') {
+									
+									echo $error;
+									
+								}
+								else {
+									
+									echo "Server is offline.";
+									
+								}
+								
+							}
+						
+						echo '</p></div>
 					</li>
 					
 					';
-					//check_circle is the check icon
-				} //$servertitle var
+				} //foreach
 			?>
 			
 		</ul>
